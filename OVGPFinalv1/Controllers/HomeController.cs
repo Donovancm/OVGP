d7ce116a06205903a465f9b346e0e05a0101cb04
Donovancm <donovan.dcm@gmail.com>
2019-06-02 18:33:51 +0200

    Nog meer features identity

--- OVGPFinalv1/Controllers/HomeController.cs
+++ OVGPFinalv1/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace OVGPFinalv1.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
