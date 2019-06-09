018f1b3abc6b4369370dec0d0aa92617109cecbf
Donovancm <donovan.dcm@gmail.com>
2019-06-09 13:23:54 +0200

    new logo en contactform updated

---
+++ OVGPFinalv1/Models/ContactFormModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OVGPFinalv1.Models
+{
+    public class ContactFormModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
