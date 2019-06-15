842d90a3191c37051c544198086bd51a4f984995
Donovancm <donovan.dcm@gmail.com>
2019-06-15 18:01:27 +0200

    Meer eng --> dutch

--- OVGPFinalv1/Models/Agenda.cs
+++ OVGPFinalv1/Models/Agenda.cs
@@ -12,10 +12,12 @@ namespace OVGPFinalv1.Models
         public int AgendaId { get; set; }
         [Required]
         public string Titel { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Tekst { get; set; }
-
         public string Label { get; set; }
+        [DataType(DataType.Date)]
         public DateTime StartDatum { get; set; }
+        [DataType(DataType.Date)]
         public DateTime EindDatum { get; set; }
     }
 }
